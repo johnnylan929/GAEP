80a044dc1eeab09619d0f7651d8b91583d02bbd2
johnnylan1314 <johnnylan1314@gmail.com>
2017-01-02 00:33:19 +0800

    v4 Listen for Auth State

--- app/src/main/java/tw/lan/myfireauthemailpass/MainActivity.java
+++ app/src/main/java/tw/lan/myfireauthemailpass/MainActivity.java
@@ -1,13 +1,49 @@
 package tw.lan.myfireauthemailpass;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("lan", "onAuthStateChanged:signed_in:" + user.getUid());
+                }else {
+                    // User is signed out
+                    Log.d("lan", "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
